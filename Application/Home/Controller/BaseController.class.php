71b68c00bb3686b4058500a7efb777e5ba650eed
Lich2013 <longzy2013@gmail.com>
2018-04-21 19:37:27 +0800

    80% ok

---
+++ Application/Home/Controller/BaseController.class.php
@@ -0,0 +1,42 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class BaseController extends Controller {
+    public function _initialize(){
+        header('Access-Control-Allow-Origin: *');
+        if (APP_DEBUG) {
+            $openid = 'ouRCyjhdsj8RQofIOPHc7nX9hA983';//session('openid');//
+            $nickname = '知识混子周政3';// session('nickname'); //
+        } else {
+            $openid = session('openid');//
+            $nickname = session('nickname'); //
+        }
+        if (!$openid || !$nickname) {
+            $openid = I('get.openid');
+            $nickname = urldecode(I('get.nickname'));//'知识混子周政';//
+        }
+        if (!$openid  || !$nickname) {
+            $uri = 'https://wx.idsbllp.cn/MagicLoop/index.php?s=/addon/Api/Api/oauth&redirect='.urlencode('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+            redirect($uri);
+        }
+        session('openid', $openid);
+        session('nickname', $nickname);
+        $users = M('users');
+        $num = $users->where(array('openid' => $openid))->count();
+        if ($num == 0) {
+            $data = array(
+                'openid' => $openid,
+                'nickname' => $nickname,
+                'imgurl' => urldecode(I('get.headimgurl')),
+            );
+            $users->add($data);
+        } else {
+            $img = I('get.headimgurl');
+            if ($nickname && $img) {
+                $data['nickname'] = $nickname;
+                $data['imgurl'] = urldecode($img);
+                $users->where(array('openid' => $openid))->save($data);
+            }
+        }
+    }
+}
---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,119 @@
+<?php
+namespace Home\Controller;
+use Org\Util\String;
+use Org\Util\Date;
+
+class IndexController extends BaseController {
+    private $appid = 'wx81a4a4b77ec98ff4';
+    private $acess_token = 'gh_68f0a1ffc303';
+    public function index() {
+        $openid = session('openid');
+        $user = M('users')->where(array('openid' => $openid))->find();
+        $signature = $this->JSSDKSignature();
+        $this->assign('nickname', $user['nickname']);
+        $this->assign('avatar', $user['imgurl']);
+        $this->assign('signature', $signature);
+        $this->assign('appid', $this->appid);
+        $this->display();
+    }
+
+    public function getcontent() {
+        $time = I('post.time');
+        $d = new Date($time);
+        if ($d->parse($time) > time()){
+           $this->ajaxReturn(array(
+               'status' => 403,
+               'info' => '非法时间'
+           ));
+        }
+        $year = $d->format("%Y");
+        $event = M('event')->where(array('year' => $year))->getField('event');
+        $story = M('story')->order('rand()')->find();
+        $this->ajaxReturn(array(
+            'status' => 200,
+            'data' => array(
+                'keyword' => $story['keyword'],
+                'days' => ceil($d->dateDiff(time())),
+                'event' => $event,
+                'describe' => $story['describe']
+            )
+        ));
+    }
+
+    public function JSSDKSignature(){
+        $string = new String();
+        $jsapi_ticket =  $this->getTicket();
+        $data['jsapi_ticket'] = $jsapi_ticket['data'];
+        $data['noncestr'] = $string->randString();
+        $data['timestamp'] = time();
+        $data['url'] = 'https://'.$_SERVER['HTTP_HOST'].__SELF__;//生成当前页面url
+        $data['signature'] = sha1($this->ToUrlParams($data));
+        return $data;
+    }
+    private function ToUrlParams($urlObj){
+        $buff = "";
+        foreach ($urlObj as $k => $v) {
+            if($k != "signature") {
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+
+    /*curl通用函数*/
+    private function curl_api($url, $data=''){
+        // 初始化一个curl对象
+        $ch = curl_init();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, 1 );
+        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+        // 运行curl，获取网页。
+        $contents = json_decode(curl_exec($ch), true);
+        // 关闭请求
+        curl_close($ch);
+        return $contents;
+    }
+
+    private function getTicket() {
+        $time = time();
+        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
+        $string='';
+        for($i=0;$i<16;$i++){
+            $num = mt_rand(0,61);
+            $string .= $str[$num];
+        }
+        $secret =sha1(sha1($time).md5($string)."redrock");
+        $t2 = array(
+            'timestamp'=>$time,
+            'string'=>$string,
+            'secret'=>$secret,
+            'token'=>$this->acess_token,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/apiJsTicket";
+        return $this->curl_api($url, $t2);
+    }
+
+    public function addtestdata(){
+        $string = new String();
+        $users = M('users');
+        $ucq = M('user_current_question');
+        for ($i=0;$i<120;$i++) {
+            $openid = $string->randString();
+            $score = rand(0, 100);
+            $data1 = array(
+                'openid' => $openid,
+                'nickname' => '周老板'.$score,
+                'score' => $score
+            );
+            $data2 = array(
+                'openid' => $openid
+            );
+            $users->add($data1);
+            $ucq->add($data2);
+        }
+    }
+}
